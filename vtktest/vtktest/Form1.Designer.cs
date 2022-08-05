0e75a241ee8297ec5837dcc43f8baaa8ae5add25
Gray Programmerz <54432616+graysuit@users.noreply.github.com>
2022-08-05 14:12:10 +0500

    Add files via upload

---
+++ vtktest/vtktest/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿using System.Windows.Forms;
+
+namespace vtktest
+{
+    public partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.renderWindowControl1 = new Kitware.VTK.RenderWindowControl();
+            this.column = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // renderWindowControl1
+            // 
+            this.renderWindowControl1.AddTestActors = false;
+            this.renderWindowControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.renderWindowControl1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.renderWindowControl1.Location = new System.Drawing.Point(162, 12);
+            this.renderWindowControl1.Name = "renderWindowControl1";
+            this.renderWindowControl1.Size = new System.Drawing.Size(601, 426);
+            this.renderWindowControl1.TabIndex = 0;
+            this.renderWindowControl1.TestText = null;
+            // 
+            // column
+            // 
+            this.column.FormattingEnabled = true;
+            this.column.Location = new System.Drawing.Point(12, 12);
+            this.column.Name = "column";
+            this.column.Size = new System.Drawing.Size(142, 21);
+            this.column.TabIndex = 28;
+            this.column.SelectedIndexChanged += new System.EventHandler(this.column_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 450);
+            this.Controls.Add(this.column);
+            this.Controls.Add(this.renderWindowControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Kitware.VTK.RenderWindowControl renderWindowControl1;
+        private ComboBox column;
+    }
+}
+
---
+++ vtktest/vtktest/Form1.cs
@@ -0,0 +1,176 @@
+﻿using Kitware.VTK;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace vtktest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<vtkActor> actors = new List<vtkActor>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            vtu("block.vtu");
+            var colms = getColumns(0);
+            foreach (var colm in colms)
+            {
+                column.Items.Add(colm);
+            }
+        }
+
+
+        public  void vtu(string filePath)
+        {
+            renderWindowControl1.Invalidate();
+            vtkXMLUnstructuredGridReader reader = vtkXMLUnstructuredGridReader.New();
+            reader.SetFileName(filePath);
+            reader.Update();
+            vtkRenderWindow renderWindow = renderWindowControl1.RenderWindow;
+            vtkRenderer renderer = renderWindow.GetRenderers().GetFirstRenderer();
+            vtkDataSetMapper mapper = vtkDataSetMapper.New();
+            mapper.SetInputConnection(reader.GetOutputPort());
+            actors.Add(new vtkActor());
+            vtkActor actor = actors.Last();
+            actor.SetMapper(mapper);
+            renderer.AddActor(actor);
+            renderer.ResetCamera();
+            renderWindowControl1.Validate();
+
+        }
+ 
+        private void column_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            renderWindowControl1.Invalidate();
+            for (int i = 0; i < actors.Count; i++)
+            {
+                var colm = column.GetItemText(column.SelectedItem);
+                setColors(i, colm);
+            }
+            renderWindowControl1.Validate();
+        }
+
+        public void setColors(int actorIndex, string column)
+        {
+            List<string> colums = new List<string>();
+            vtkActor actor = actors[actorIndex];
+            var mapper = actor.GetMapper();
+            var output = mapper.GetInputAsDataSet();
+
+
+            var colors = vtkUnsignedCharArray.New();
+            colors.SetNumberOfComponents(3);
+            colors.SetName("Colors");
+
+            var cell = output.GetCellData();
+            var num1 = cell.GetNumberOfComponents();
+            var num2 = cell.GetNumberOfArrays();
+
+
+            for (int i = 0; i < num1; i++)
+            {
+                var cells = cell.GetArray(i);
+                if (cells == null) continue;
+                if (column != cell.GetArrayName(i)) continue;
+                var range = cell.GetArray(i).GetRange();
+
+
+                //Console.WriteLine("\n\n\n");
+                for (int j = 0; j < cells.GetNumberOfTuples(); j++)
+                {
+                    var val = cells.GetComponent(i, j);
+                    var per = val / cell.GetArray(i).GetRange()[1];
+                    if (!isNotNan(val)) per = 0;
+                    Console.WriteLine($"{val}:{per}");
+                    // Console.WriteLine(val);
+                    var c =  InterpolateColor(per); //get color
+                    var R = c.R;
+                    var G = c.G;
+                    var B = c.B;
+                    colors.InsertNextTuple3((double)R, (double)G, (double)B);
+                    colors.InsertNextTuple3((double)R, (double)G, (double)B);
+                }
+
+            }
+            output.GetPointData().SetScalars(colors);
+            output.GetCellData().SetScalars(colors);
+        }
+
+        public bool isNotNan(double value)
+        {
+            return !double.IsNaN(value) && !double.IsInfinity(value);
+        }
+
+        Color[] colors = new Color[] { Color.Red, Color.Lime, Color.Orange, Color.Blue };
+
+        public Color InterpolateColor(double x)
+        {
+            double r = 0.0, g = 0.0, b = 0.0;
+            double total = 0.0;
+            double step = 1.0 / (double)(colors.Length - 1);
+            double mu = 0.0;
+            double sigma_2 = 0.035;
+            foreach (Color color in colors)
+            {
+                total += Math.Exp(-(x - mu) * (x - mu) / (2.0 * sigma_2)) / Math.Sqrt(2.0 * Math.PI * sigma_2);
+                mu += step;
+            }
+
+            mu = 0.0;
+            foreach (Color color in colors)
+            {
+                double percent = Math.Exp(-(x - mu) * (x - mu) / (2.0 * sigma_2)) / Math.Sqrt(2.0 * Math.PI * sigma_2);
+                mu += step;
+
+                r += color.R * percent / total;
+                g += color.G * percent / total;
+                b += color.B * percent / total;
+            }
+
+            return Color.FromArgb(255, (int)r, (int)g, (int)b);
+        }
+
+
+
+        public double[] getRange(int actorIndex, string column)
+        {
+            List<string> colums = new List<string>();
+            vtkActor actor = actors[actorIndex];
+            var output = actor.GetMapper().GetInputAsDataSet();
+            var cell = output.GetCellData();
+            for (int i = 0; i < cell.GetNumberOfArrays(); i++)
+            {
+                var colm = cell.GetArrayName(i);
+                var arr = cell.GetArray(i);
+                if (column == colm)
+                {
+                    if (arr == null) return null;
+                    return arr.GetRange();
+                }
+            }
+            return null;
+        }
+
+        public List<string> getColumns(int actorIndex)
+        {
+            List<string> colums = new List<string>();
+            vtkActor actor = actors[actorIndex];
+            var output = actor.GetMapper().GetInputAsDataSet();
+            var cell = output.GetCellData();
+            for (int i = 0; i < cell.GetNumberOfArrays(); i++)
+            {
+                var colm = cell.GetArrayName(i);
+                if (colm != null) colums.Add(colm);
+            }
+            return colums;
+        }
+
+
+    } 
+}
